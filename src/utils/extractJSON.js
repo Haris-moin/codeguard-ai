899d030e5cf13ce125e1b3579df318d289af971d
muhammad.haris.moin <harismoin48@gmail.com>
2026-04-16 16:14:52 +0500

    testing ai reviewer via dummy commit

--- src/utils/extractJSON.js
+++ src/utils/extractJSON.js
@@ -4,6 +4,7 @@ export const extractJSON = (text) => {
     console.log('match: ', match);
     if (!match) return [];
 
+    console.log('JSON.parse(match[0]);: ', JSON.parse(match[0]));
     return JSON.parse(match[0]);
   } catch {
     return [];
