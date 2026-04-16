aad71c11efc29b172335d0088170039abef3ec15
muhammad.haris.moin <harismoin48@gmail.com>
2026-04-16 14:24:40 +0500

    testing ai reviewer vi dummy commit

--- src/controllers/webhook.controller.js
+++ src/controllers/webhook.controller.js
@@ -1,6 +1,7 @@
 import { processPullRequest } from "../services/pr.service.js";
 
 export const handleWebhook = async (req, res) => {
+  console.log('req: for ai reviewer', req);
   try {
     const event = req.headers["x-github-event"];
     console.log('tesing events', event);
